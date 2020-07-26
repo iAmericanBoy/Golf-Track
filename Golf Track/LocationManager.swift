c21ce00c6eb6802c01b036498dec95cfda9c80c2
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2020-07-26 16:14:18 -0400

    Adds LocationManager

---
+++ Golf Track/LocationManager.swift
@@ -0,0 +1,14 @@
+//
+//  LocationManager.swift
+//  Golf Track
+//
+//  Created by Dominic Lanzillotta on 7/26/20.
+//
+
+import CoreLocation
+
+class LocationManager {
+    static let shared = CLLocationManager()
+
+    private init() {}
+}
