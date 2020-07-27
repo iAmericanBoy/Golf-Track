81d2932a5605a7b8f54443756d7e64e16cb6b429
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2020-07-26 20:45:03 -0400

    Adds LocationManger

--- Golf Track/LocationManager.swift
+++ Golf Track/LocationManager.swift
@@ -7,8 +7,58 @@
 
 import CoreLocation
 
-class LocationManager {
-    static let shared = CLLocationManager()
+enum LocationManagerError: Error {
+    case notAllowed
+    case unknown
+}
 
-    private init() {}
+class LocationManager: NSObject {
+    private static var manager = CLLocationManager()
+
+    static let shared = LocationManager()
+
+    var completion: ((Result<Void, LocationManagerError>) -> Void)?
+
+    override private init() {
+        super.init()
+        LocationManager.manager.delegate = self
+    }
+
+    func requestPermission() {
+        switch LocationManager.manager.authorizationStatus() {
+        case .notDetermined:
+            LocationManager.manager.requestAlwaysAuthorization()
+        case .restricted, .denied:
+            completion?(.failure(.notAllowed))
+        case .authorizedAlways:
+            completion?(.success(()))
+        case .authorizedWhenInUse:
+            completion?(.success(()))
+            @unknown default:
+            completion?(.failure(.unknown))
+            assertionFailure()
+        }
+    }
+
+    func startLocationUpdates() {
+        LocationManager.manager.activityType = .fitness
+        LocationManager.manager.distanceFilter = 10
+        LocationManager.manager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            requestPermission()
+        case .restricted, .denied, .authorizedWhenInUse:
+            completion?(.failure(.notAllowed))
+        case .authorizedAlways:
+            completion?(.success(()))
+        @unknown default:
+            completion?(.failure(.unknown))
+            assertionFailure()
+        }
+    }
 }
