dc9269739afcc8dd6f9e61d8ea49740c7ba5b595
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2020-07-27 19:18:03 -0400

    Adds End trip support

--- Golf Track/TripManager.swift
+++ Golf Track/TripManager.swift
@@ -8,7 +8,6 @@
 import CoreLocation
 
 class TripManager {
-    
     private var startDate: Date?
     private var distance: Measurement<UnitLength>?
     private var locationList: [CLLocation] = []
@@ -18,7 +17,8 @@ class TripManager {
     init(locationManager: LocationManager = LocationManager.shared) {
         self.locationManager = locationManager
     }
-        
+    
+    /// Checks if Location permission has benn given and Starts Trip after permission was givven
     func startTrip() {
         locationManager.completion = { [self] permissionResult in
             switch permissionResult {
@@ -30,8 +30,19 @@ class TripManager {
         }
     }
     
+    /// ends a Trip
+    func endTrip() {
+        end()
+    }
+    
     private func start() {
-        self.locationManager.startLocationUpdates()
-        self.startDate = Date()
+        locationManager.startLocationUpdates()
+        startDate = Date()
+    }
+    
+    private func end() {
+        locationManager.endLocationUpdates()
+        // calculate Time
+        // calculate Distance
     }
 }
