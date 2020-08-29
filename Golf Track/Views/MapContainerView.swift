15fbb258120e9719586cffa09ec6e49581f02662
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2020-08-29 11:18:08 -0400

    Makes the MapContainerView use more SwiftUI features

--- Golf Track/Views/MapContainerView.swift
+++ Golf Track/Views/MapContainerView.swift
@@ -10,18 +10,40 @@ import SwiftUI
 
 struct MapContainerView: UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
-        let mapView = MKMapView(frame: UIScreen.main.bounds)
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
         return mapView
     }
 
-    func updateUIView(_ uiView: MKMapView, context: Context) {
+    func updateUIView(_ view: MKMapView, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapContainerView
+
+        init(_ parent: MapContainerView) {
+            self.parent = parent
+        }
     }
 }
 
-struct MapView_Previews: PreviewProvider {
+struct MapContainerView_Previews: PreviewProvider {
     static var previews: some View {
         MapContainerView()
+    }
+}
+
+extension MKPointAnnotation {
+    static var example: MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Home to the 2012 Summer Olympics."
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: -0.13)
+        return annotation
     }
 }
