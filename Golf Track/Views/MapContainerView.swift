eb185897298a8ce768e8b7dc98468f17aad54b74
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2020-09-01 08:04:41 -0400

    Map containerview can now be reusable and won't need its own view model anymore

--- Golf Track/Views/MapContainerView.swift
+++ Golf Track/Views/MapContainerView.swift
@@ -9,19 +9,30 @@ import MapKit
 import SwiftUI
 
 struct MapContainerView: UIViewRepresentable {
-    @ObservedObject var viewModel: MapContainerViewModel
+    @Binding var overlays: [MKOverlay]
+    @Binding var center: CLLocationCoordinate2D
+    @Binding var mapType: MKMapType
 
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
         mapView.delegate = context.coordinator
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
+        mapView.mapType = mapType
+        mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 500, longitudinalMeters: 500)
         return mapView
     }
 
     func updateUIView(_ view: MKMapView, context: Context) {
-        if view.overlays.count != viewModel.overlays.count {
-            view.addOverlays(viewModel.overlays)
+        if view.overlays.count != overlays.count {
+            view.addOverlays(overlays)
+        }
+        if view.region.center.latitude.magnitude != center.latitude.magnitude {
+            let region = MKCoordinateRegion(center: center, latitudinalMeters: 250, longitudinalMeters: 250)
+            view.setRegion(region, animated: true)
+        }
+        if view.mapType != mapType {
+            view.mapType = mapType
         }
     }
 
@@ -41,7 +52,7 @@ struct MapContainerView: UIViewRepresentable {
                 return MKOverlayRenderer(overlay: overlay)
             }
             let renderer = MKPolylineRenderer(polyline: polyline)
-            renderer.strokeColor = .blue
+            renderer.strokeColor = .white
             renderer.lineWidth = 3
             return renderer
         }
@@ -50,7 +61,7 @@ struct MapContainerView: UIViewRepresentable {
 
 struct MapContainerView_Previews: PreviewProvider {
     static var previews: some View {
-        MapContainerView(viewModel: MapContainerViewModel(tripManager: MockTripManager()))
+        MapContainerView(overlays: .constant(TestingVariations.simpleLine), center: .constant(TestingVariations.center), mapType: .constant(.satellite))
     }
 }
 
@@ -60,6 +71,10 @@ private extension MapContainerView_Previews {
             let start = CLLocationCoordinate2D(latitude: CLLocationDegrees(39.976273), longitude: CLLocationDegrees(-83.005588))
             let end = CLLocationCoordinate2D(latitude: CLLocationDegrees(39.976310), longitude: CLLocationDegrees(-83.005443))
             return [MKPolyline(coordinates: [start, end], count: 2)]
+        }
+
+        static var center: CLLocationCoordinate2D {
+            return CLLocationCoordinate2D(latitude: CLLocationDegrees(39.976273), longitude: CLLocationDegrees(-83.005588))
         }
     }
 }
